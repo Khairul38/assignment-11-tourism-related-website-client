ea4a7b51ec18d67910a25aaee72322e79a4de0cf
KHAIRUL ISLAM <khairulislam38@gmail.com>
2022-12-12 19:45:20 +0600

    update

--- src/Hooks/useDestinations/useDestinations.js
+++ src/Hooks/useDestinations/useDestinations.js
@@ -1,13 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const useDestinations = () => {
-    const [destinations, setDestinations] = useState([]);
-    useEffect(() => {
-        fetch('https://wicked-nightmare-49756.herokuapp.com/destinations')
-            .then(res => res.json())
-            .then(data => setDestinations(data));
-    }, []);
-    return { destinations };
+  const [destinations, setDestinations] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://assignment-11-tourism-related-website-server.vercel.app/destinations"
+    )
+      .then((res) => res.json())
+      .then((data) => setDestinations(data));
+  }, []);
+  return { destinations };
 };
 
 export default useDestinations;
--- src/Hooks/useServices/useServices.js
+++ src/Hooks/useServices/useServices.js
@@ -1,14 +1,16 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
 
 const useServices = () => {
-    const [services, setServices] = useState([]);
-    useEffect(() => {
-        fetch('https://wicked-nightmare-49756.herokuapp.com/packages')
-            .then(res => res.json())
-            .then(data => setServices(data));
-    }, []);
-    return { services };
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://assignment-11-tourism-related-website-server.vercel.app/packages"
+    )
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return { services };
 };
 
 export default useServices;
